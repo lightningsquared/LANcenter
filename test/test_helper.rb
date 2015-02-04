5fcf71427b34ae090169ba4012c1f41eb53aebfc
Ariel Kirkwood <ariel.kirkwood@me.com>
2015-02-04 15:30:50 -0500

    Further config for Travis/Code Climate

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,11 +1,13 @@
+if ENV['CODECLIMATE_REPO_TOKEN']
+  require 'codeclimate-test-reporter'
+  CodeClimate::TestReporter.start
+end
 ENV['RAILS_ENV'] ||= 'test'
 require File.expand_path('../../config/environment', __FILE__)
 require 'rails/test_help'
 require "minitest/reporters"
-require "codeclimate-test-reporter"
-
 Minitest::Reporters.use!
-CodeClimate::TestReporter.start
+
 
 class ActiveSupport::TestCase
   # Setup all fixtures in test/fixtures/*.yml for all tests in alphabetical order.
