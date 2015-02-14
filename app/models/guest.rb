962b10808753be854e042547a6728e50e2cb90f4
zackhanni <zack.hanni@gmail.com>
2015-02-14 02:31:26 -0500

    added validations

--- app/models/guest.rb
+++ app/models/guest.rb
@@ -1,5 +1,7 @@
 class Guest < ActiveRecord::Base
   belongs_to :event
-
+  validates :name,  presence: true 
+  validates :email, presence: true, uniqueness: { case_sensitive: false }
+  validates :checked_in_at, presence: true, if: :checked_in?
   enum attendance_status: [ :not_checked_in, :checked_in, :checked_out ]
 end
